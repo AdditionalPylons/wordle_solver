ae2e79fcb69af1729e013194fe075e0d297ba6a9
Bill <bill@popstack.io>
2022-01-23 14:34:55 -0600

    Implement DataState enum

--- coffee_shop.py
+++ coffee_shop.py
@@ -1,25 +1,33 @@
+from enum import Enum
 from pathlib import Path
 import re
 
 guess1 = ['_', 'i', 'r', '_', 't']
 
-def solve_wordle(possible_answers, guess_results):
+class DataState(Enum):
+    white = "empty"
+    grey = "absent"
+    yellow = "present"
+    green = "correct"
+
+
+def solve_wordle(answer_dict, guess, results):
 
 
     results = []
 
-    for answer in possible_answers:
-        if len(answer) != len(guess_results):
-            possible_answers.remove(answer)
+    for answer in answer_dict:
+        if len(answer) != len(guess):
+            answer_dict.remove(answer)
         
 
-    for answer in possible_answers:    
+    for answer in answer_dict:    
         valid = True    
         for n in range(len(guess1)):
-            if guess_results[n] == '_':
+            if guess[n] == '_':
                 pass
             else:
-                if answer[n] != guess_results[n]:
+                if answer[n] != guess[n]:
                     valid = False
                     break
         if valid:
@@ -34,4 +42,4 @@ capitals = re.compile('[A-Z]')
 
 with open("/home/bill/Code/python_code/wordle_solver/word_list.txt") as word_list:
     words = [word.strip() for word in word_list.readlines() if len(word.strip()) == 5 and not capitals.match(word[0])]
-    solve_wordle(possible_answers=words, guess_results=guess1)
+    solve_wordle(answer_dict=words, guess=guess1, results=None)
